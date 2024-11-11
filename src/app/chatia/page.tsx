a909474991ae91aab0605aa1b1425e897356c420
Paulo-V-Developer34 <165535750+Paulo-V-Developer34@users.noreply.github.com>
2024-11-11 14:16:51 -0300

    chatia is getting blog texts by mistake

--- src/app/chatia/page.tsx
+++ src/app/chatia/page.tsx
@@ -5,15 +5,19 @@ import prisma from "@/lib/db";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default async function Blogs() {
+export default function Blogs() {
   const [chat,setchat] = useState<any>("")
+
+  useEffect(()=>{
+    setchat(prisma.post.findMany().then())
+  },[])
 
     return (
       <div className="flex flex-col items-center">
         <h1 className="font-bold text-2xl">Aqui você pode encontrar muitos blogs legais</h1>
-        <h2 className="font-bold text-xl">Quatidade de posts ({posts.length})</h2>
+        <h2 className="font-bold text-xl">Quatidade de posts ({chat?.length})</h2>
         {
-          posts?.map(el=>{
+          chat?.map((el: any)=>{
             return <section key={el.id} className="m-3 p-1 bg-slate-400 rounded w-2/4">
               <Link href={`/blog/${el.slug}`}><h1 className="font-bold">{el.title}</h1></Link>
               <p>{el.content}</p>
